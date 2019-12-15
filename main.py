48a5d5a5b7002065222422814920e9277f140f78
TGEPhoto <56611468+TGEPhoto@users.noreply.github.com>
2019-12-15 14:13:55 +0100

    Fixed a typo

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
 @client.command()
-@commands.is_owner
+@commands.is_owner()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
