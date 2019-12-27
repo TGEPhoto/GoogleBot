7465212516da7df9ae2f7d30dcdeae8dc9e6a13e
TGEPhoto <56611468+TGEPhoto@users.noreply.github.com>
2019-12-27 21:07:24 +0100

    Added ban and kick commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -9,7 +9,56 @@ class Moderation(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def purge(self, ctx, amount=2):
+        """
+        Purges given amount of messages
+        """
         await ctx.channel.purge(limit=amount)
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        """
+        Bans a user and dms the reason
+        """
+        msg = "You have been banned from snootyboop land"
+        if reason:
+            msg += f" for {reason}"
+
+        try:
+            await member.send(msg)
+        except discord.Forbidden:
+            await ctx.send("I can't message that user")
+
+        await ctx.send(f'{member.name}#{member.discriminator} has been banned!')
+        await member.ban(reason=reason)
+    
+    @ban.error
+    async def do_repeat_handler(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("You didn't give me a member to ban!")
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        """
+        Kicks a user and dms the reason
+        """
+        msg = "You have been kicked from snootyboop land"
+        if reason:
+            msg += f" for {reason}"
+
+        try:
+            await member.send(msg)
+        except discord.Forbidden:
+            await ctx.send("I can't message that user")
+
+        await ctx.send(f'{member.name}#{member.discriminator} has been kicked!')
+        await member.kick(reason=reason)
+
+    @kick.error
+    async def do_repeat_handler(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("You didn't give me a member to kick!")
 
 def setup(client):
     client.add_cog(Moderation(client))
