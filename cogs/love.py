6b017444fd4124371cd9a6b24436168b2883630a
TGEPhoto <56611468+TGEPhoto@users.noreply.github.com>
2019-12-27 21:08:38 +0100

    Update love.py

--- cogs/love.py
+++ cogs/love.py
@@ -9,14 +9,18 @@ class Love(commands.Cog):
 
     @commands.command(aliases=["3"])
     async def love(self, ctx):
+        """
+        Sends random love messages
+        """
         responses = ['You are beautiful!',
                     'You are epic',
                     'I love you uwu',
                     'You are wonderful',
                     'I love you cutie',
                     'You are cute',
-                    'ily comrade']
-        await ctx.send(random.choice(responses))
+                    'ily comrade',
+                    'Message my creator to add more love!']
+        await ctx.send(f'{random.choice(responses)}')
 
 def setup(client):
     client.add_cog(Love(client))
