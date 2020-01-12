927435b70e4e5beb1f4495902cb93fc6fd5544a1
TGEPhoto <56611468+TGEPhoto@users.noreply.github.com>
2020-01-12 22:04:14 +0100

    Thank you Lee!

---
+++ fun.py
@@ -0,0 +1,82 @@
+import discord
+import random
+from discord.ext import commands
+
+RES_DIR = 'res'
+
+
+class Fun(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def say(self, ctx, *, message):
+        """
+        Makes the bot say something
+        """
+        await ctx.channel.trigger_typing()
+        sentMessage = await ctx.send(message)
+        await ctx.message.delete()
+
+    @commands.command()
+    async def kidnap(self, ctx, member: discord.Member):
+        """
+        Apparently kidnaps the member
+        """
+        await ctx.send(f'\N{IMP}\N{PISTOL} Get in the van, kiddo! {member.name}#{member.discriminator}')
+
+    @kidnap.error
+    async def kidnap_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('You gotta mention someone!')
+
+    @commands.command()
+    async def tip(self, ctx):
+        """
+        Sends random tips
+        """
+        with open(f'{RES_DIR}/tips.txt', 'r') as f:
+            await ctx.send(random.choice(f.readlines()))
+
+    @commands.command()
+    async def joke(self, ctx):
+        """
+        Sends terrible dad jokes
+        """
+        with open(f'{RES_DIR}/joke.txt', 'r') as f:
+            await ctx.send(random.choice(f.readlines()))
+    
+    @commands.command()
+    async def anthem(self, ctx):
+        """
+        Sends USSR anthem
+        """
+        await ctx.send('https://www.youtube.com/watch?v=U06jlgpMtQs')
+
+    @commands.command()
+    async def cookie(self, ctx, member: discord.Member):
+        """
+        Sends a cookie to the user
+        """
+        await ctx.send(f'\N{COOKIE}\N{COOKIE}\N{COOKIE} {member.name}#{member.discriminator} has received a cookie from {ctx.author.mention}! *nom nom nom*')
+
+    @cookie.error
+    async def cookie_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("You didn't tell me who to give the cookie! Maybe you should get cookies instead <3")
+
+    @commands.command()
+    async def boop(self, ctx, member: discord.Member):
+        """
+        Boops a member
+        """
+        await ctx.send(f'*boop boop* {ctx.author.mention} has booped {member.name}#{member.discriminator}!')
+
+    @boop.error
+    async def boop_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("You didn't tell me who to boop! Maybe we should boop you instead?")
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
